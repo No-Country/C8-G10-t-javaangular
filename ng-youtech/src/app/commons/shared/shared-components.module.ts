9bb1e3359b02e0e62bd63b00c3de2a83a8d29840
jimmy <jimmyhd_19@hotmail.com>
2022-11-17 19:29:07 -0500

    firts

---
+++ ng-youtech/src/app/commons/shared/shared-components.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CardCreatorComponent } from '../components/card-creator/card-creator.component';
+
+@NgModule({
+  declarations: [CardCreatorComponent],
+  imports: [],
+  exports: [CardCreatorComponent],
+})
+export class SharedComponentsModule {}
