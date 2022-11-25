eba210275056cabca2c9b1acfc8b1c46cf83337a
jimmy <jimmyhd_19@hotmail.com>
2022-11-24 19:26:52 -0500

    se termina el componente de detalle

--- ng-youtech/src/app/business/home/home-flow/components/creator-content-detail/creator-content-detail.module.ts
+++ ng-youtech/src/app/business/home/home-flow/components/creator-content-detail/creator-content-detail.module.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { AvatarComponent } from '../../../../../commons/components/avatar/avatar.component';
 import { CreatorContentDetailComponent } from './creator-content-detail.component';
@@ -7,7 +10,14 @@ import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [CreatorContentDetailComponent, DetailComponent],
-  imports: [CommonModule, MatTabsModule, AvatarComponent],
+  imports: [
+    CommonModule,
+    MatTabsModule,
+    AvatarComponent,
+    MatButtonModule,
+    MatIconModule,
+    MatChipsModule,
+  ],
   exports: [CreatorContentDetailComponent],
 })
 export class CreatorContentDetailModule {}
